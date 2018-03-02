ba567f59f0ef310121e404828995c9e814683321
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-03-02 13:48:52 +0530

    Update 43(mar2).C

--- 43(mar2).C
+++ 43(mar2).C
@@ -2,20 +2,19 @@
 #include<string.h>
 int main(void) 
 {
-	char str1[50],str2[50];
-	int i,j;
-	printf("enter the string1:");
-	scanf("%s",str1);
-	printf("\n enter the string2:");
-	scanf("%s",str2);
-	for(i=0;str1[i]!='\0';i++)
-	{
+char str1[50],str2[50];
+int i,j;	printf("enter the string1:");
+scanf("%s",str1);
+printf("\n enter the string2:");
+scanf("%s",str2);
+for(i=0;str1[i]!='\0';i++)
+{
 		
-	}
-	for(j=0;str2[j]!='\0';j++)
-	{
-		str1[i]=str2[j];
-		i++;
+}
+for(j=0;str2[j]!='\0';j++)
+{
+	str1[i]=str2[j];
+	i++;
 	}
 	str1[i]='\0';
 	printf("\n%s",str1);
