ebfde042773c9604009e1492c7777b4f850f0298
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-03-02 13:49:49 +0530

    Update 44(mar2).C

--- 44(mar2).C
+++ 44(mar2).C
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 int main(void) 
 {
-	int n;
-	printf("enter the number:");
-	scanf("%d",&n);
-	if(n>=1 && n<=10)
-	{
-		printf("\n yes");
-	}
-	else
-	{
-		printf("\n no");
-	}
-	return 0;
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n>=1 && n<=10)
+{
+printf("\n yes");
+}
+else	{
+printf("\n no");
+}
+return 0;
 }
