131c5150cca5fabf82af20e82e6451c06bc2f2dd
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-03-02 13:47:43 +0530

    Update 42(mar2).C

--- 42(mar2).C
+++ 42(mar2).C
@@ -5,14 +5,12 @@ int main(void)
 	char str1[50],str2[50];
 	int n1,n2;
 	printf("enter the string1:");
-	scanf("%s",str1);
-	printf("enter the string2:");
-	scanf("%s",str2);
-	n1=strlen(str1);
-	n2=strlen(str2);
-	if(n1>n2)
+scanf("%s",str1);
+printf("enter the string2:");
+	scanf("%s",str2);	n1=strlen(str1);
+	n2=strlen(str2);	if(n1>n2)
 	{
-		printf("\n%s",str1);
+	printf("\n%s",str1);
 	}
 	else if(n2>n1)
 	{
